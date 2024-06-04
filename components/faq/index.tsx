2f8626ff0951ad1955f2d8c80b1e36956a269a6d
anis2566 <anichuranis1000@gmail.com>
2024-06-04 14:05:30 +0600

    faafa

--- components/faq/index.tsx
+++ components/faq/index.tsx
@@ -26,13 +26,13 @@ export const FAQ = () => {
                 <AccordionItem value="item-3" className="border-primary">
                     <AccordionTrigger>Can I actually win money on Fatafat Bazar?</AccordionTrigger>
                     <AccordionContent>
-                        Yes you can. It's a number game and based on prediction you can win and be rewarded.
+                        Yes you can. It&apos;s a number game and based on prediction you can win and be rewarded.
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="item-4" className="border-primary">
                     <AccordionTrigger>Is it safe to add money to Fatafat Bazar?</AccordionTrigger>
                     <AccordionContent>
-                        It's 100 % safe to add money into wallet. You can withdraw your money at any point of time.
+                        It&apos;s 100 % safe to add money into wallet. You can withdraw your money at any point of time.
                     </AccordionContent>
                 </AccordionItem>
                 <AccordionItem value="item-5" className="border-primary">
